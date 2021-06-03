33f6ab82097acda6ce93c82db62bf54d9184832f
matthewgagne1 <>
2021-06-03 14:10:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = 0
+
+document.getElementById('button').addEventListener('click',addition)
+
+function addition () {
+  number = document.getElementById('text').value
+
+  number = parseInt(number)
+
+  number = number * number
+
+  alert(number)
+}
